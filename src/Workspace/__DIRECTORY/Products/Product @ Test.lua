4f5992ed8afb2056e5d00bf2566ba7e8a2a34d08
Conner <cpait14@gmail.com>
2025-08-12 18:54:21 -0500

    Update sourcemap.json to reflect new module structure and file paths, including updates to Client and Server libraries, and added modules for Game and Plot directories.

---
+++ src/Workspace/__DIRECTORY/Products/Product @ Test.lua
@@ -0,0 +1,22 @@
+--!strict
+
+local RunService = game:GetService("RunService")
+
+return {
+	ProductId = 3354009677,
+	DisplayName = "Car Purchase",
+	Icon = "",
+	Description = "Test",
+	OneTimePurchase = false,
+	Callback = function(player: Player)
+		-- local Saving = require(game.ServerScriptService.Library.Saving)
+
+		-- local save = Saving.Get(player)
+		-- if not save then
+		-- 	return false
+		-- end
+
+		-- save.Coins += 75_000
+		return true
+	end,
+}
