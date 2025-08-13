0f3186e52b5bed711623519a3f3771a4f637eace
Conner <cpait14@gmail.com>
2025-08-13 11:47:30 -0500

    Update plots submodule to latest commit c8873db; revise sourcemap.json to reflect updated module paths and structure changes across Client and Server libraries.

---
+++ src/StarterPlayer/StarterPlayerScripts/PlayerBillboards.client.lua
@@ -0,0 +1,31 @@
+--!strict
+
+local Functions = require(game.ReplicatedStorage.Library.Functions)
+local ClientPlot = require(game.ReplicatedStorage.Plot.ClientPlot)
+
+ClientPlot.Created:Connect(function(plot: ClientPlot.Type)
+    local model = plot:WaitModel()
+    local owner = plot:GetOwner()
+    local billboard: BillboardGui = model:WaitForChild("PlayerBillboard"):WaitForChild("BillboardGui")::BillboardGui
+   
+    local frame = billboard:WaitForChild("Frame")::Frame
+    local name = frame:WaitForChild("Name")::TextLabel
+    local multi = frame:WaitForChild("Multi")::TextLabel
+    local playerIcon = frame:WaitForChild("PlayerIcon")::ImageLabel
+
+    task.spawn(function()
+        local icon = Functions.GetAvatarFromUserIdAsync(owner.UserId, Enum.ThumbnailType.HeadShot, Enum.ThumbnailSize.Size100x100)
+        
+        if icon then
+            playerIcon.Image = icon
+
+            local loading = playerIcon:WaitForChild("Loading")::ImageLabel
+            loading.Visible = false
+        end
+    end)
+
+    name.Text = owner.DisplayName
+    multi.Text = "x1 Multi"
+
+    billboard.Enabled = true
+end)
