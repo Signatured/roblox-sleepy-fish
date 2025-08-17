7d187eb76614135345f4f45a04ee694ed18c4e51
Signatured <cpait14@gmail.com>
2025-08-17 11:47:02 -0500

    Update framework submodule reference and improve fish spawning mechanics
    - Updated the framework submodule reference to the latest commit.
    - Enhanced fish spawning mechanics to balance fish counts and improve gameplay dynamics.
    - Refactored fish generation logic for better clarity and maintainability.

---
+++ src/ServerScriptService/Commands/GiveGadget.lua
@@ -0,0 +1,31 @@
+--!strict
+
+local ServerScriptService = game:GetService("ServerScriptService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local CommandManager = require(ServerScriptService.CommandManager)
+local CommandType = require(ReplicatedStorage.Game.Library.Types.Commands)
+local GadgetManager = require(ServerScriptService.Game.Library.Gadgets)
+
+local Command = {
+	Name = "GiveGadget",
+	Aliases = {"gg", "givegadget"},
+	Permissions = {"Admin", "Owner", "Tester", "Developer"}, 
+	Parameters = {
+		{Type = "Player", Name = "TargetPlayer", Optional = true},
+		{Type = "String", Name = "GadgetId"},
+	} :: {CommandType.Parameter},
+
+	Execute = function(player, args)
+		local targetPlayers = args[1]
+        local gadgetId = args[2]
+
+		for _, targetPlayer in ipairs(targetPlayers) do
+			GadgetManager.Give(targetPlayer, gadgetId:gsub("_", " "))
+		end
+	end,
+} :: CommandType.Command
+
+CommandManager.Register(Command)
+
+return {} 
