f1c0068835c8fcfdeac2f4af84ad727c03c8eab0
FlourTortilla <anthonyh.dev@gmail.com>
2025-08-21 12:29:06 -0500

    Add fish sound effects and notifications for catching and grabbing fish
    - Introduced a new client script to handle audio feedback and notifications when a fish is caught or grabbed in water, enhancing player engagement.
    - Implemented sound playback for both events, providing immediate auditory confirmation.
    - Added notification messages to inform players of their actions, improving clarity and interaction during gameplay.

---
+++ src/StarterPlayer/StarterPlayerScripts/FishSFX.client.lua
@@ -0,0 +1,34 @@
+--!strict
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Audio = require(ReplicatedStorage.Library.Audio)
+local NotificationCmds = require(ReplicatedStorage.Library.Client.NotificationCmds)
+local Network = require(ReplicatedStorage.Library.Client.Network)
+
+-- Listen for server event when a fish is successfully banked
+Network.Fired("Fish_Caught", function(displayName: string)
+	Audio.Play("rbxassetid://94238694593476", script, 1, 0.3)
+	if typeof(displayName) == "string" and #displayName > 0 then
+		NotificationCmds.Message(`You caught a {displayName}!`, {
+			Color = Color3.fromRGB(0, 255, 0),
+			Time = 3,
+		})
+	end
+end)
+
+Network.Fired("Fish_Grabbed_In_Water", function(fishId: string)
+	print("HERE!!!!", fishId)
+	Audio.Play("rbxassetid://85747710232715", script, 1, 0.3)
+
+	if typeof(fishId) == "string" and #fishId > 0 then
+		NotificationCmds.Message(`You grabbed a {fishId}!`, {
+			Color = Color3.fromRGB(0, 255, 0),
+			Time = 3,
+		})
+	end
+end)
+
+
+
+
