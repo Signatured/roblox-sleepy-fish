7374cddbd1abc18da083b736062536de9eb479df
Conner <cpait14@gmail.com>
2025-08-22 11:54:13 -0500

    Update PreloadSounds.client.lua to include additional sound assets
    - Added multiple sound asset IDs to the preloadSounds table for enhanced audio experience in the game.
    - Ensured all new sounds are properly preloaded to improve performance during gameplay.

--- src/StarterPlayer/StarterPlayerScripts/PreloadSounds.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/PreloadSounds.client.lua
@@ -3,7 +3,11 @@
 local Audio = require(game.ReplicatedStorage.Library.Audio)
 
 local preloadSounds = {
-    
+    "rbxassetid://139871930665325",
+    "rbxassetid://107509119621196",
+    "rbxassetid://83561525465892",
+    "rbxassetid://85226925115980",
+    "rbxassetid://117601096719800"
 }
 
 Audio.PreloadSounds(preloadSounds)
