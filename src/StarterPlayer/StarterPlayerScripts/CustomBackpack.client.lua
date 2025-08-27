b749762f18b283e21b776b71de8939bfc200043e
Conner <cpait14@gmail.com>
2025-08-27 05:56:33 -0500

    Add backpack visibility toggle on RenderStepped
    - Implemented a connection to RunService.RenderStepped to disable the CoreGuiType.Backpack in StarterGui, enhancing player experience by managing backpack visibility during gameplay.
    - Ensured proper module organization by requiring Satchel from the appropriate path at the top of the file.

--- src/StarterPlayer/StarterPlayerScripts/CustomBackpack.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/CustomBackpack.client.lua
@@ -1,3 +1,10 @@
 --!strict
 
+local RunService = game:GetService("RunService")
+local StarterGui = game:GetService("StarterGui")
+
 local Satchel = require(game.ReplicatedStorage.Game.Modules.Satchel)
+
+RunService.RenderStepped:Connect(function()
+    StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false)
+end)
