dcf4704ae3261706ffb87ced2c7dd56723500ebd
Conner <cpait14@gmail.com>
2025-09-30 15:39:16 -0500

    chore: update subproject commit and add FishTypes to LuckyBlocks module
    - Updated subproject commit reference in the plots directory.
    - Added FishTypes dependency to LuckyBlocks module to enhance fish-related data structures.
    - Ensured all modifications adhere to project standards and architecture.

--- src/ReplicatedStorage/Library/Types/LuckyBlocks.lua
+++ src/ReplicatedStorage/Library/Types/LuckyBlocks.lua
@@ -2,6 +2,7 @@
 
 local DirectoryTypes = require(game.ReplicatedStorage.Game.Library.Types.Directory)
 local RarityTypes = require(game.ReplicatedStorage.Game.Library.Types.Rarity)
+local FishTypes = require(game.ReplicatedStorage.Game.Library.Types.Fish)
 
 local module = {}
 
@@ -9,6 +10,11 @@ export type raw_dir = {
 	DisplayName: string,
 	Rarity: RarityTypes.dir_schema,
     Loot: {[string]: number},
+}
+
+export type lucky_block_visual_data = {
+    FishId: string,
+    Type: FishTypes.fish_type,
 }
 
 export type dir_schema = raw_dir & DirectoryTypes.dir_schema
