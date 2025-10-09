3b509f27b81bac48e9dea27875a90477912dc005
Conner <cpait14@gmail.com>
2025-10-09 18:14:51 -0500

    chore: update sourcemap.json to reflect current project structure
    - Revised the sourcemap.json file to accurately represent the latest project organization and module hierarchy.
    - Ensured all modifications adhere to project standards and architecture.

---
+++ src/Workspace/__DIRECTORY/Fish/Fish 902 @ Nightmare Maja/init.lua
@@ -0,0 +1,25 @@
+--!strict
+
+local FishTypes = require(game.ReplicatedStorage.Game.Library.Types.Fish)
+local Rarity = require(game.ReplicatedStorage.Game.Library.Directory.Rarity)
+
+return {
+    DisplayName = "Nightmare Maja",
+    Icon = "rbxassetid://99968950359680",
+    MutationIcons = {
+        Bloodfish = "rbxassetid://124483285860707",
+        Galaxy = "rbxassetid://79709413745812",
+    },
+    Rarity = Rarity.Exclusive,
+    MoneyPerSecond = 150,
+    BaseUpgradeCost = 10000,
+    BillboardOffset = 13,
+    PedestalOffset = -3,
+    RarityWeight = 0,
+    -- IndexOffset = 8,
+    -- IndexPositionOffset = Vector3.new(-7, 2, 0),
+    BestFishMultiplier = 2,
+}::FishTypes.raw_dir
+
+
+
