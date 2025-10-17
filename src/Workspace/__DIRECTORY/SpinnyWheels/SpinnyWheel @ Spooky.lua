ab306e09b58b9c464ade9ccfacbcf0e546bb7c66
Conner <cpait14@gmail.com>
2025-10-17 16:18:51 -0500

    fix: update Spooky wheel icon for enhanced visual representation
    - Changed the icon asset ID for the Spooky wheel to a new asset ID to improve its visual appeal.
    - Ensured all modifications adhere to project standards and architecture.

--- src/Workspace/__DIRECTORY/SpinnyWheels/SpinnyWheel @ Spooky.lua
+++ src/Workspace/__DIRECTORY/SpinnyWheels/SpinnyWheel @ Spooky.lua
@@ -11,7 +11,7 @@ return {
 			Index = 1,
 			Weight = 1,
 			DisplayChance = "1%",
-			Icon = "rbxassetid://99825068045387",
+			Icon = "rbxassetid://122931667882291",
 			Title1 = "Spooky",
 			Title2 = "Squid",
 			Title3 = "$1.6k/s",
