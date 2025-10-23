b1000f437262796e3a647fa8dc589b61e2d490ea
Conner <cpait14@gmail.com>
2025-10-23 17:18:22 -0500

    fix: update sourcemap.json to reflect project structure changes
    - Adjusted the sourcemap.json file to accurately represent the current project structure and module organization.
    - Ensured all modifications adhere to project standards and architecture.

---
+++ src/Workspace/__DIRECTORY/Fish/Fish 903 @ Octobloop/init.lua
@@ -0,0 +1,26 @@
+--!strict
+
+local FishTypes = require(game.ReplicatedStorage.Game.Library.Types.Fish)
+local Rarity = require(game.ReplicatedStorage.Game.Library.Directory.Rarity)
+
+return {
+    DisplayName = "Octobloop",
+    Icon = "rbxassetid://82803321939240",
+    MutationIcons = {
+        Bloodfish = "rbxassetid://93368172717234",
+        Galaxy = "rbxassetid://126624016705908",
+        Spooky = "rbxassetid://77237730484469",
+    },
+    Rarity = Rarity.Exclusive,
+    MoneyPerSecond = 150,
+    BaseUpgradeCost = 10000,
+    BillboardOffset = 11,
+    PedestalOffset = -4,
+    RarityWeight = 0,
+    -- IndexOffset = 8,
+    -- IndexPositionOffset = Vector3.new(-7, 2, 0),
+    BestFishMultiplier = 2,
+}::FishTypes.raw_dir
+
+
+
