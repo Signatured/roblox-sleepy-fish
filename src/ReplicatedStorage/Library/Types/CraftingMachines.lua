e9dd3c7522c65a3c7caf87201d3fe1253513d401
Conner <cpait14@gmail.com>
2025-10-30 18:52:46 -0500

    fix: update sourcemap.json for accurate project structure
    - Modified sourcemap.json to ensure accurate file paths and structure for various modules and scripts.
    - Ensured all modifications adhere to project standards and architecture.

---
+++ src/ReplicatedStorage/Library/Types/CraftingMachines.lua
@@ -0,0 +1,29 @@
+--!strict
+
+local DirectoryTypes = require(game.ReplicatedStorage.Game.Library.Types.Directory)
+local FishTypes = require(game.ReplicatedStorage.Game.Library.Types.Fish)
+
+local module = {}
+
+export type raw_dir = {
+	DisplayName: string,
+	RecipeResetTime: number,
+	Recipes: {recipe_data_schema},
+}
+
+export type recipe_data_schema = {
+	FishResult: FishTypes.create_params?, -- if provided, use this over RarityResult
+	RarityResult: string, -- use as backup for FishResult if FishResult is not provided
+	CraftTime: number,
+	CraftCost: number,
+	RequiredIngredientAmount: number,
+}
+
+export type recipe_ingredients_schema = {
+	Result: FishTypes.create_params,
+	Ingredients: {FishTypes.create_params},
+}
+
+export type dir_schema = raw_dir & DirectoryTypes.dir_schema
+
+return module
